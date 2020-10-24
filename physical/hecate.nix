5f7b4aaca5cdc2971bfca3f5342970512ca02eb0
Raito Bezarius <masterancpp@gmail.com>
2020-10-25 00:36:39 +0200

    hecate: add target host

--- physical/hecate.nix
+++ physical/hecate.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   deployment.targetEnv = "none";
+  deployment.targetHost = "hecate.v6.lahfa.xyz";
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/0f9f1a1c-c225-469b-81be-ea4bf4b58455";
